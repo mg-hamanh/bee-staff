b83b89429571240427e93b6e64c02cd31331832b
Andrew <support@andrew.work.gd>
2025-08-25 10:08:35 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
+import {  headers } from "next/headers";
 
 export async function middleware(request: NextRequest) {
-  const session = await auth.api.getSession({
-    headers: request.headers,
-  });
   
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+    
 
   const url = new URL(request.url);
   const path = url.pathname;
@@ -18,7 +20,8 @@ export async function middleware(request: NextRequest) {
   const isPublicPath = publicPaths.some((p) => path.startsWith(p));
 
   // If there's no session and the user is not on a public path, redirect them to sign-in 
-  if (!session && !isPublicPath) { return NextResponse.redirect(new URL("/sign-in", request.url)); }
+  if (!session && !isPublicPath) {     
+    return NextResponse.redirect(new URL("/sign-in", request.url)); }
 
   // If there is a session and the user tries to access /sign-in, redirect them to the home page
   if (session && path === "/sign-in") {
@@ -32,3 +35,4 @@ export const config = {
   runtime: "nodejs",
   matcher: [ "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|svg|gif|webp|ico)).*)"], // Apply to all routes except static, image, and favicon
 };`   `
+
